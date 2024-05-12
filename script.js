c6dad57d94ea4998fdefd9733781a340567ae023
Kalidhrona <97901008+Kalidhrona@users.noreply.github.com>
2024-05-12 21:10:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to open the popup
+window.onload = function() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+// Function to close the popup
+function closePopup() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
